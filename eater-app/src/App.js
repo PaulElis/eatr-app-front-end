b5c91ecdd6d1bb692b3429d693a2c2b4f6565388
github username <github email address>
2018-08-07 09:57:50 -0400

    placed search and sign on in header

--- eater-app/src/App.js
+++ eater-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, Form } from 'semantic-ui-react'
+import { Button, Form, Input, Menu } from 'semantic-ui-react'
 
 
 import BusinessesContainer from './containers/BusinessesContainer'
@@ -11,7 +11,36 @@ class App extends Component {
     currentUser: {},
     searchRan: false,
     searchTerm: "",
-    location: ""
+    location: "",
+    username: "",
+  }
+
+  handleSearchSubmit = event => {
+    event.preventDefault()
+    this.props.runSearch(this.state.searchTerm)
+      this.setState({
+        searchTerm: "",
+      })
+  }
+
+  handleSearchChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleUserSubmit = event => {
+    event.preventDefault()
+      this.setState({
+        username: ""
+      })
+    this.props.createUser(this.state.username)
+  }
+
+  handleUserChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   setCurrentUser = (user) => {
@@ -33,11 +62,25 @@ class App extends Component {
       <main>
       <div className="App">
         <header>
-          <p id='home-header'><b>Eatr</b></p>
-            <Form id='form' onSubmit = {this.handleSubmit}>
-              <Form.Input icon='search' id='input' size='mini' name="searchTerm" placeholder="Find restaurants, bars..." value={this.state.searchTerm} onChange={this.handleChange}></Form.Input>
-              <Button id='search-button' color='grey' compact fluid size='mini' type='submit'>Search</Button>
-            </Form>
+            <Menu secondary>
+                <Menu.Menu position='center'>
+                  <Menu.Item>
+                    <p id='home-header'>
+                      <b>Eatr</b>
+                    </p>
+                  </Menu.Item>
+                </Menu.Menu>
+                <Menu.Menu position='right'>
+                  <Menu.Item id='form' onSubmit = {this.handleSearchSubmit}>
+                    <Input icon='search' id='input' size='small' name="searchTerm" placeholder="Find restaurants, bars.." value={this.state.searchTerm} onChange={this.handleSearchChange}></Input>
+                    {/* <Button id='search-button' color='grey' compact fluid size='small' type='submit'>Search</Button> */}
+                  </Menu.Item>
+                  <Menu.Item onSubmit={this.handleUserSubmit}>
+                    <Input icon='users' iconPosition='left' size='small' name="username" placeholder="Username" value={this.state.username} onChange={this.handleUserChange} />
+                    <Button id='login-register-button' color='grey' fluid compact size='large' type='submit'>Sign On</Button>
+                  </Menu.Item>
+                </Menu.Menu>
+            </Menu>
         </header>
           <BusinessesContainer currentUser={this.state.currentUser} searchTerm={this.state.searchTerm} location={this.state.location} setCurrentUser={this.setCurrentUser} runSearch={this.runSearch}/>
       </div>
