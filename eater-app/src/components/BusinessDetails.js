33878bea46381c4fbbe43613ac2b2e1a45d2f2f4
github username <elis.paul7@gmail.com>
2018-08-09 09:49:21 -0400

    cleaned up businessdetails component

--- eater-app/src/components/BusinessDetails.js
+++ eater-app/src/components/BusinessDetails.js
@@ -31,49 +31,7 @@ class BusinessDetails extends React.Component {
 
   render() {
 
-    // const { active } = this.state
-    // const content = (
-    //   <div className="right floated header">
-    //     <i className="big heart icon"></i>
-    //   </div>
-    // )
 return (
-      // <div className="ui max width centered raised link cards">
-      // <div className="card" style={{height: '10 rem'}}>
-      //   <Dimmer.Dimmable
-      //     as={Image}
-      //     dimmed={active}
-      //     dimmer={{ active, content }}
-      //     onMouseEnter={this.handleShow}
-      //     onMouseLeave={this.handleHide}
-      //     size='medium'
-      //     src={this.props.image} onClick={this.handleClick} alt={this.props.index}
-      //   />
-      //
-      //     <div className="content" id='card'>
-      //       <div className="header" id='header'>
-      //         <a href={this.props.business.url} target="_blank">{this.props.business.name}</a>
-      //       </div>
-      //       <div className="meta" id='meta'>
-      //         {this.props.business.cuisine}
-      //       </div>
-      //       <div className="description" id='description'>
-      //         {this.props.business.address}<br />
-      //         {this.props.business.city}, {this.props.business.state} {this.props.business.zip_code}
-      //         <ReviewsList business={this.props.business} reviews={this.props.reviews} deleteReview={this.props.deleteReview}/>
-      //         <NewReviewForm currentUser={this.props.currentUser} business={this.props.business} addReview={this.props.addReview} deleteReview={this.deleteReview}/>
-      //       </div>
-      //     </div>
-      //   <div className="extra content" id='extra-content'>
-      //     <span className="left floated">
-      //       {this.props.business.review_count} Reviews / Rating: {this.props.business.rating}/5
-      //     </span>
-      //     <span className="right floated">
-      //       {this.props.business.price}
-      //     </span>
-      //   </div>
-      // </div>
-      // </div>
       <Card.Group centered>
       <Card fluid color='red'>
         <br></br>
